59826bb806585d937a80b0c57579ffa99b2f84b2
CarlsonTSAI <carlson52411@gmail.com>
2019-06-19 09:04:57 +0800

    2019/06/19

--- js/brad04.js
+++ js/brad04.js
@@ -1,12 +1,40 @@
-let var1 = 10 , var2 = 3;
+
+let var1 = 10, var2 = 3;
 let var3 = var1 / var2;
 document.write(`${var1}/${var2}=${var3}`);
 
 var show = document.getElementById('show')
 
-function eq(){
+function eq() {
+    let op = document.getElementById('op');
+    let opVal = op.selectedIndex;
     let x = document.getElementById('x').value
     let y = document.getElementById('y').value
-    let tot = `${parseInt(x/y)} ....${x%y}`
-    show.innerHTML = tot
+    let nx =  parseInt(x)
+    let ny = parseInt(y)
+
+    let tot 
+
+    switch (opVal) {
+        case 0:
+            tot = nx + ny
+            show.innerHTML = tot
+            break;
+        case 1:
+            tot = parseInt(x - y)
+            show.innerHTML = tot
+            break;
+        case 2:
+            tot = parseInt(x * y)
+            show.innerHTML = tot
+            break;
+        case 3:
+            tot =  `${parseInt(x/y)} ....${x%y}`
+            show.innerHTML = tot
+            break;
+    }
+     
 }
+
+
+	
